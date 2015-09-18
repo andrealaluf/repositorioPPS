e9ff481537bb136bbc51f8f6f5f71a1bab30059f
fabionazzi <fabio_nazzi090@hotmail.com>
2015-09-17 21:22:03 -0300

    agrega comentario

--- Command.py
+++ Command.py
@@ -2,6 +2,7 @@ from Message import Message
 
 class Command (Message):
 	
+	# Constructor
 	def __init__(self, senderID, command,value):
 		Message.__init__(self,senderID)
 		self.senderID = senderID
